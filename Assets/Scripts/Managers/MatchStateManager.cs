893dba4ddb7df4f0dc6dcb75ed42cd419d3044b2
Burak Kurt <burakkurt2010@gmail.com>
2024-04-02 00:57:05 +0300

    feat : Frame advancing

--- Assets/Scripts/Managers/MatchStateManager.cs
+++ Assets/Scripts/Managers/MatchStateManager.cs
@@ -1,19 +1,51 @@
 using System;
+using UnityEngine;
 using Utilities;
 
 namespace Managers
 {
-    public class MatchStateManager: MonoSingleton<MatchStateManager>
+    public class MatchStateManager : MonoSingleton<MatchStateManager>
     {
         public event Action<FrameData> OnFrameDataChanged;
-        public void TogglePlayback(bool b)
+
+        private FrameData CurrentFrame { get; set; }
+        private int _currentFrameIndex;
+
+        private float _timeSinceLastFrameChange = 0f;
+        private const float TimePerFrame = 0.03f;
+        private bool _isPlaying = true;
+
+        void Update()
         {
-            throw new System.NotImplementedException();
+            if (!_isPlaying || MatchDataManager.Instance.allFrameData.Count == 0) return;
+
+            _timeSinceLastFrameChange += Time.deltaTime;
+            if (Math.Abs(_timeSinceLastFrameChange) >= TimePerFrame)
+            {
+                _timeSinceLastFrameChange = 0f;
+                AdvanceFrame();
+            }
         }
 
-        public void UpdateGameState(FrameData initialFrameData)
+        private void AdvanceFrame()
         {
-            throw new System.NotImplementedException();
+            var frameCount = MatchDataManager.Instance.allFrameData.Count;
+
+            _currentFrameIndex = (_currentFrameIndex + 1) % frameCount;
+            CurrentFrame = MatchDataManager.Instance.allFrameData[_currentFrameIndex];
+            
+            OnFrameDataChanged?.Invoke(CurrentFrame);
+        }
+
+        public void UpdateGameState(FrameData frameData)
+        {
+            CurrentFrame = frameData;
+            OnFrameDataChanged?.Invoke(frameData);
+        }
+        
+        public void TogglePlayback(bool play)
+        {
+            _isPlaying = play;
         }
     }
 }
