d8e8b45fc248274fcb624cdaf78dd17c27220bd7
Burak Kurt <burakkurt2010@gmail.com>
2024-04-02 13:32:43 +0300

    refactor: top-level person animation

--- Assets/Scripts/GamePlay/Controllers/PersonController.cs
+++ Assets/Scripts/GamePlay/Controllers/PersonController.cs
@@ -8,42 +8,52 @@ namespace GamePlay.Controllers
     public class PersonController : MonoBehaviour, ITopLevelController
     {
         [SerializeField] private Animator animator;
-        private static readonly int SpeedHash = Animator.StringToHash("Speed");
-        private static readonly int IsRunningHash = Animator.StringToHash("isRunning");
-
-        [SerializeField] private float runningSpeedThreshold = 3f;
-        private bool _lastIsRunningState;
-        private float _lastSpeed = -1;
+        [SerializeField] private TopLevelPersonAnimaVariables topLevelPersonAnimaVariables;
 
         public void ApplyTopLevelChanges(IInterpolatedStateData interpolatedStateData)
         {
-            PersonData personData = (PersonData)interpolatedStateData;
-            AdjustAnimationBasedOnSpeed(personData.Speed);
+            if (interpolatedStateData is PersonData personData)
+            {
+                AdjustAnimationBasedOnSpeed(personData.Speed);
+            }
         }
-
 
         private void AdjustAnimationBasedOnSpeed(float newSpeed)
         {
-            if (!animator)
+            if (animator == null)
             {
                 Debug.LogWarning($"Animator component not found on {gameObject.name}, unable to adjust animation.");
                 return;
             }
 
-            bool isRunning = newSpeed >= runningSpeedThreshold;
-            if (isRunning != _lastIsRunningState)
-            {
-                animator.SetBool(IsRunningHash, isRunning);
-                _lastIsRunningState = isRunning;
-            }
+            bool isRunning = newSpeed >= topLevelPersonAnimaVariables.runningSpeedThreshold;
+            animator.SetBool(TopLevelPersonAnimaVariables.IsRunningHash, isRunning);
 
-            // Calculate adjusted speed only if necessary to minimize processing.
-            if (Math.Abs(newSpeed - _lastSpeed) > 0.05f)
+            // avoid recalculating if unnecessary
+            if (Math.Abs(animator.GetFloat(TopLevelPersonAnimaVariables.SpeedHash) - newSpeed) >
+                topLevelPersonAnimaVariables.speedCalculationTolerance)
             {
-                float adjustedSpeed = isRunning ? newSpeed * 0.1f : newSpeed * 0.4f;
-                animator.SetFloat(SpeedHash, adjustedSpeed);
-                _lastSpeed = newSpeed;
+                animator.SetFloat(TopLevelPersonAnimaVariables.SpeedHash, CalculateAdjustedSpeed(newSpeed, isRunning));
             }
         }
+
+        private float CalculateAdjustedSpeed(float speed, bool isRunning)
+        {
+            return isRunning
+                ? speed * topLevelPersonAnimaVariables.runningSpeedMultiplier
+                : speed * topLevelPersonAnimaVariables.walkingSpeedMultiplier;
+        }
+    }
+
+    [Serializable]
+    public class TopLevelPersonAnimaVariables
+    {
+        public float runningSpeedThreshold = 2.5f;
+        public float speedCalculationTolerance = 0.1f;
+        public float walkingSpeedMultiplier = 0.1f;
+        public float runningSpeedMultiplier = 0.4f;
+
+        public static readonly int SpeedHash = Animator.StringToHash("Speed");
+        public static readonly int IsRunningHash = Animator.StringToHash("isRunning");
     }
 }
